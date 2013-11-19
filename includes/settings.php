9af825db1e349d8053248aa55fa0fa1e65a3aea3
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-11-19 09:29:14 +0200

    Put all settings in one file.

--- includes/settings.php
+++ includes/settings.php
@@ -4,10 +4,39 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
+ * Check if site owner have checked to show extra user info.
+ *
+ * @access      public
+ * @since       1.0
+ */
+function edd_paytrail_show_extra_user_info() {
+
+	global $edd_options;
+	
+	return isset( $edd_options['edd_paytrail_show_user_info'] );
+	
+}
+
+/**
+ * Check if site owner have checked to show address fields.
+ *
+ * @access      public
+ * @since       1.0
+ */
+function edd_paytrail_show_extra_address_fields() {
+
+	global $edd_options;
+	
+	return isset( $edd_options['edd_paytrail_show_address_fields'] );
+	
+}
+
+/**
  * Register the paytrail.fi gateway settings
  *
  * @access      public
  * @since       1.0
+ * @param 		$settings array the existing plugin settings
  * @return      array
  */
 
@@ -54,5 +83,48 @@ function edd_paytrail_add_settings( $settings ) {
 
 }
 add_filter( 'edd_settings_gateways', 'edd_paytrail_add_settings' );
+
+/**
+ * Registers the new options in Extensions.
+ *
+ * @access      public
+ * @since       1.0
+ * @param 		$settings array the existing plugin settings
+ * @return      array
+*/
+function edd_paytrail_settings( $settings ) {
+
+	$extensions_settings = array(
+		array(
+			'id' => 'edd_paytrail_header',
+			'name' => '<strong>' . __( 'Paytrail', 'edd-paytrail' ) . '</strong>',
+			'desc' => '',
+			'type' => 'header',
+			'size' => 'regular'
+		),
+		array(
+			'id' => 'edd_paytrail_show_image',
+			'name' => __( 'Show Paytrail image', 'edd-paytrail' ),
+			'desc' => __( 'Check this if you want to show Paytrail image on checkout page.', 'edd-paytrail' ),
+			'type' => 'checkbox'
+		),
+		array(
+			'id' => 'edd_paytrail_show_user_info',
+			'name' => __( 'Additional user info', 'edd-paytrail' ),
+			'desc' => __( 'Check this if you want to show phone number and company name fields on checkout page. This info is also send to Paytrail account.', 'edd-paytrail' ),
+			'type' => 'checkbox'
+		),
+		array(
+			'id' => 'edd_paytrail_show_address_fields',
+			'name' => __( 'Show address fields', 'edd-paytrail' ),
+			'desc' => __( 'Check this if you want to show address fields on checkout page. Address and product info is also send to Paytrail account.', 'edd-paytrail' ),
+			'type' => 'checkbox'
+		)
+	);
+
+	return array_merge( $settings, $extensions_settings );
+
+}
+add_filter( 'edd_settings_extensions', 'edd_paytrail_settings' );
 
 ?>
