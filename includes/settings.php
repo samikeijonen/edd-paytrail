40e95f3a5221008b5bf593681b13395895a65b1b
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-01-08 18:09:27 +0200

    Remove check to show extra user info.

--- includes/settings.php
+++ includes/settings.php
@@ -4,21 +4,6 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Check if site owner have checked to show extra user info.
- *
- * @access      public
- * @since       1.0.0
- * @return      boolean
- */
-function edd_paytrail_show_extra_user_info() {
-
-	global $edd_options;
-	
-	return isset( $edd_options['edd_paytrail_show_user_info'] );
-	
-}
-
-/**
  * Check if site owner have checked to show address fields.
  *
  * @access      public
@@ -46,38 +31,38 @@ function edd_paytrail_gateways_settings( $settings ) {
 
 	$paytrail_settings = array(
 		array(
-			'id'    => 'edd_paytrail_settings',
-			'name'  => '<strong>' . _x( 'Paytrail Settings', 'Paytrail settings in Gateways page', 'edd-paytrail' ) . '</strong>',
-			'desc'  => __( 'Configure the Paytrail settings', 'edd-paytrail' ),
-			'type'  => 'header'
+			'id'   => 'edd_paytrail_settings',
+			'name' => '<strong>' . _x( 'Paytrail Settings', 'Paytrail settings in Gateways page', 'edd-paytrail' ) . '</strong>',
+			'desc' => __( 'Configure the Paytrail settings', 'edd-paytrail' ),
+			'type' => 'header'
 		),
 		array(
-			'id'    => 'edd_paytrail_merchant_id',
-			'name'  => __( 'Merchant ID', 'edd-paytrail' ),
-			'desc'  => __( 'Enter your Paytrail Merchant ID. This is needed in order to take payment.', 'edd-paytrail' ),
-			'type'  => 'text',
-			'size'  => 'regular'
+			'id'   => 'edd_paytrail_merchant_id',
+			'name' => __( 'Merchant ID', 'edd-paytrail' ),
+			'desc' => __( 'Enter your Paytrail Merchant ID. This is needed in order to take payment.', 'edd-paytrail' ),
+			'type' => 'text',
+			'size' => 'regular'
 		),
 		array(
-			'id'    => 'edd_paytrail_merchant_secret',
-			'name'  => __( 'Merchant Secret', 'edd-paytrail' ),
-			'desc'  => __( 'Enter your Paytrail Merchant Secret. This is needed in order to take payment.', 'edd-paytrail' ),
-			'type'  => 'text',
-			'size'  => 'regular'
+			'id'   => 'edd_paytrail_merchant_secret',
+			'name' => __( 'Merchant Secret', 'edd-paytrail' ),
+			'desc' => __( 'Enter your Paytrail Merchant Secret. This is needed in order to take payment.', 'edd-paytrail' ),
+			'type' => 'text',
+			'size' => 'regular'
 		),
 		array(
-			'id'    => 'edd_paytrail_test_merchant_id',
-			'name'  => __( 'Test Merchant ID', 'edd-paytrail' ),
-			'desc'  => __( 'Enter your Paytrail test Merchant ID.', 'edd-paytrail' ),
-			'type'  => 'text',
-			'size'  => 'regular'
+			'id'   => 'edd_paytrail_test_merchant_id',
+			'name' => __( 'Test Merchant ID', 'edd-paytrail' ),
+			'desc' => __( 'Enter your Paytrail test Merchant ID.', 'edd-paytrail' ),
+			'type' => 'text',
+			'size' => 'regular'
 		),
 		array(
-			'id'    => 'edd_paytrail_test_merchant_secret',
-			'name'  => __( 'Test Merchant Secret', 'edd-paytrail' ),
-			'desc'  => __( 'Enter your Paytrail test Merchant Secret.', 'edd-paytrail' ),
-			'type'  => 'text',
-			'size'  => 'regular'
+			'id'   => 'edd_paytrail_test_merchant_secret',
+			'name' => __( 'Test Merchant Secret', 'edd-paytrail' ),
+			'desc' => __( 'Enter your Paytrail test Merchant Secret.', 'edd-paytrail' ),
+			'type' => 'text',
+			'size' => 'regular'
 		)
 	);
 
@@ -98,26 +83,20 @@ function edd_paytrail_extensions_settings( $settings ) {
 
 	$extensions_settings = array(
 		array(
-			'id' => 'edd_paytrail_header',
+			'id'   => 'edd_paytrail_header',
 			'name' => '<strong>' . _x( 'Paytrail Settings', 'Paytrail settings in Extensions page', 'edd-paytrail' ) . '</strong>',
 			'desc' => '',
 			'type' => 'header',
 			'size' => 'regular'
 		),
 		array(
-			'id' => 'edd_paytrail_show_image',
+			'id'   => 'edd_paytrail_show_image',
 			'name' => __( 'Show Paytrail image', 'edd-paytrail' ),
 			'desc' => __( 'Check this if you want to show Paytrail image on checkout page.', 'edd-paytrail' ),
 			'type' => 'checkbox'
 		),
 		array(
-			'id' => 'edd_paytrail_show_user_info',
-			'name' => __( 'Additional user info', 'edd-paytrail' ),
-			'desc' => __( 'Check this if you want to show phone number and company name fields on checkout page. This info is also send to Paytrail account if you use Finnish address fields below.', 'edd-paytrail' ),
-			'type' => 'checkbox'
-		),
-		array(
-			'id' => 'edd_paytrail_show_address_fields',
+			'id'   => 'edd_paytrail_show_address_fields',
 			'name' => __( 'Finnish address fields', 'edd-paytrail' ),
 			'desc' => __( 'Check this if you want to show address fields like in Finland on checkout page. Address and product info is also send to Paytrail account in this case.', 'edd-paytrail' ),
 			'type' => 'checkbox'
